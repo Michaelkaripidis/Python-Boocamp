07896a16cf22bcc8c091badb1502f78ecf1f4a82
My Name <mikle.karibov@inbox.ru>
2021-04-16 17:02:37 +0300

    daily

---
+++ Week 2/Day3/daily.js
@@ -0,0 +1,13 @@
+var S0 = "";
+    for (i = 0; i <= 5; i++) {
+        S0 = S0 + "*";
+        console.log(S0);
+    }
+
+// for (i = 0; i <= 5; i++) {
+//     var S = "";
+//     for (n = 0; n <= i; n++) {
+//         S = S + "*";
+//         console.log(S)    
+//     }    
+// }
